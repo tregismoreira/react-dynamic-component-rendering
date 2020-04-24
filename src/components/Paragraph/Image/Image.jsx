fa869025ac2f0c69fbfe3ac9011974138348875e
Thiago Régis <tregismoreira@gmail.com>
2020-04-24 17:54:12 -0300

    Set proptype for alt attribute in <img>; Disable eslint.

--- src/components/Paragraph/Image/Image.jsx
+++ src/components/Paragraph/Image/Image.jsx
@@ -3,14 +3,16 @@ import PropTypes from 'prop-types';
 
 import styles from './Image.module.scss';
 
-const Image = (props) => <img {...props} className={styles.image} />;
+const Image = (props) => <img {...props} className={styles.image} />; // eslint-disable-line
 
 Image.propTypes = {
   src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
 };
 
 Image.defaultProps = {
-  src: 'http://lorempixel.com/500/200/',
+  src: 'http://lorempixel.com/500/200',
+  alt: 'Sample image',
 };
 
 export default Image;
