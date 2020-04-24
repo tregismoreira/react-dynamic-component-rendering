c9b669fd96214ae7231603a9f6396f6e2473571d
Thiago Régis <tregismoreira@gmail.com>
2020-04-24 17:12:22 -0300

    New Undefined paragraph type.

---
+++ src/components/Paragraph/Undefined/Undefined.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './Undefined.module.scss';
+
+const Undefined = () => (
+  <div className={styles.wrapper}>Undefined paragraph type.</div>
+);
+
+export default Undefined;
