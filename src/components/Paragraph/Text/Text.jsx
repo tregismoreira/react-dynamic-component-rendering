57f3d302feb9d3e8d5e1d8697a5df23a70801806
Thiago Régis <tregismoreira@gmail.com>
2020-04-24 17:39:20 -0300

    Set proptype to Text component

--- src/components/Paragraph/Text/Text.jsx
+++ src/components/Paragraph/Text/Text.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Text = ({ content }) => <p>{content}</p>;
+
+Text.propTypes = {
+  content: PropTypes.string.isRequired,
+};
 
 export default Text;
